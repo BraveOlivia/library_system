4ee50809fd362a0c5baeae80e334bba4cea62847
Olivia <zhouxuanolivia@gmail.com>
2025-05-09 08:11:07 +1000

    add documentation

--- library_system.py
+++ library_system.py
@@ -1,7 +1,7 @@
 import sys
 # print (sys.argv)
 
-
+# Book class with five attributes and a method to print out the detailed info of each book.
 class Book:
     def __init__(self,id,title,author,year):
         self.id=id
@@ -15,32 +15,39 @@ class Book:
 
 books=[]
 
+# Add a book to the books array
 def add_book(id,title,author,year):
     book=Book(id,title,author,year)
     books.append(book)
     return book
 
+# Borrow a book with the unique id
 def borrow_book(id):
     for book in books:
         if str(book.id)==id and book.available==True:
             book.available = False
             return book
             
-
+# Return a book with the id
 def return_book(id):
     for book in books:
         if str(book.id)==id:
             book.available = True
 
+# List all available books
 def list_available():
     for book in books:
         if book.available == True:
             print(book)
-# book=Book(123,"harry potter","JK rowing",2011)
-# add_book(123,"harry potter","JK rowing",2011)
-# add_book("124","mindset","unkown",2014)
-# print(books)
-# print("........")
-# list_available()
-# borrow_book("123")
-# list_available()
+
+
+# Test demo
+book=Book(123,"harry potter","JK rowing",2011)
+add_book(123,"harry potter","JK rowing",2011)
+add_book("124","mindset","unkown",2014)
+print(books)
+list_available()
+borrow_book("123")
+list_available()
+return_book("123")
+list_available()
--- test.py
+++ test.py
@@ -4,23 +4,23 @@ import library_system
 class TestLibrary(unittest.TestCase):
 
     def test_book(self):
-        book1=library_system.Book(123,"Harry Potter","J. K. Rowling",1997)
+        book1=library_system.Book("123","Harry Potter","J. K. Rowling",1997)
         self.assertEqual(book1.author,"J. K. Rowling")
 
     def test_add_book(self):
-        book2=library_system.add_book(124,"Mindsets","Carol S Dweck",2008)
+        book2=library_system.add_book("124","Mindsets","Carol S Dweck",2008)
         self.assertEqual(book2.year,2008)
 
     def test_borrow_book(self):
-        book2=library_system.add_book(124,"Mindsets","Carol S Dweck",2008)
-        book2=library_system.borrow_book(124)
-        self.assertEqual(book2,None)
+        book2=library_system.add_book("124","Mindsets","Carol S Dweck",2008)
+        book2=library_system.borrow_book("124")
+        self.assertEqual(book2.available,False)
 
 
     def test_return_book(self):
-        book2=library_system.add_book(124,"Mindsets","Carol S Dweck",2008)
-        book2=library_system.borrow_book(124)
-        library_system.return_book(124)
+        book2=library_system.add_book("124","Mindsets","Carol S Dweck",2008)
+        book2=library_system.borrow_book("124")
+        library_system.return_book("124")
         self.assertEqual(book2.available, True)
 
 if __name__ == '__main__':
